b93860ef8da12935da4a18e08e94ce7d380a62c4
Thamizhvaanan Ilango <thamizhlegend@gmail.com>
2021-01-31 15:04:34 +0530

    updated

--- app.py
+++ app.py
@@ -2,97 +2,118 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import random
 
-app= Flask(__name__)
-api=Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-MedRecN = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5, 37.5), 1)),
-    "Blood Pressure":random.randint(80,120),
-    "Respiration":random.randint(12,16),
-    "Glucose":random.randint(72,140),
-    "Heart Rate":random.randint(60,100),
-    "Cholesterol":random.randint(125,200),
-    "Oxygen Saturation":random.randint(95,100),
-}
 
-Diabetes = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5,37.5),1)),
-    "Blood Pressure":random.randint(80,120),
-    "Respiration":random.randint(12,16),
-    "Glucose":random.randint(200,350),
-    "Heart Rate":random.randint(60,100),
-    "Cholesterol":random.randint(125,200),
-    "Oxygen Saturation":random.randint(95,100)
-    
-}
+def MedRecN():
+    MedRecN = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(80, 120),
+        "Respiration": random.randint(12, 16),
+        "Glucose": random.randint(72, 140),
+        "Heart Rate": random.randint(60, 100),
+        "Cholesterol": random.randint(125, 200),
+        "Oxygen Saturation": random.randint(95, 100),
+    }
+    return MedRecN
 
-Prediabetes = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5,37.5),1)),
-    "Blood Pressure":random.randint(80,120),
-    "Respiration":random.randint(12,16),
-    "Glucose":random.randint(140,199),
-    "Heart Rate":random.randint(60,100),
-    "Cholesterol":random.randint(125,200),
-    "Oxygen Saturation":random.randint(95,100)
-    
-}
 
-Bronchiectasis = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5, 37.5), 1)),
-    "Blood Pressure":random.randint(90,120),
-    "Respiration":random.randint(40,60),
-    "Glucose":random.randint(72,140),
-    "Heart Rate":random.randint(60,100),
-    "Cholesterol":random.randint(125,200),
-    "Oxygen Saturation":random.randint(95,100),
-}
+def Diabetes():
+    Diabetes = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(80, 120),
+        "Respiration": random.randint(12, 16),
+        "Glucose": random.randint(200, 350),
+        "Heart Rate": random.randint(60, 100),
+        "Cholesterol": random.randint(125, 200),
+        "Oxygen Saturation": random.randint(95, 100)
 
-CongenitalHeartDefect = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5, 37.5), 1)),
-    "Blood Pressure":random.randint(90,120),
-    "Respiration":random.randint(12,16),
-    "Glucose":random.randint(72,140),
-    "Heart Rate":random.randint(45,60),
-    "Cholesterol":random.randint(200,270),
-    "Oxygen Saturation":random.randint(95,100),
-}
+    }
+    return Diabetes
 
-Hypoxemia = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5, 37.5), 1)),
-    "Blood Pressure":random.randint(90,120),
-    "Respiration":random.randint(12,16),
-    "Glucose":random.randint(72,140),
-    "Heart Rate":random.randint(60,100),
-    "Cholesterol":random.randint(125,200),
-    "Oxygen Saturation":random.randint(50,96),
-}
 
-AcuteAsthma = {
-    "Steps":random.randint(4000, 10000),
-    "Body Temperature":(round(random.uniform(35.5, 37.5), 1)),
-    "Blood Pressure":random.randint(90,120),
-    "Respiration":random.randint(20,30),
-    "Glucose":random.randint(72,140),
-    "Heart Rate":random.randint(100,125),
-    "Cholesterol":random.randint(125,200),
-    "Oxygen Saturation":random.randint(92,95),
-}
+def Prediabetes():
+    Prediabetes = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(90, 120),
+        "Respiration": random.randint(12, 16),
+        "Glucose": random.randint(140, 199),
+        "Heart Rate": random.randint(60, 100),
+        "Cholesterol": random.randint(125, 200),
+        "Oxygen Saturation": random.randint(95, 100)
 
-parser=reqparse.RequestParser()
+    }
+    return Prediabetes
+
+
+def Bronchiectasis():
+    Bronchiectasis = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(90, 120),
+        "Respiration": random.randint(40, 60),
+        "Glucose": random.randint(72, 140),
+        "Heart Rate": random.randint(60, 100),
+        "Cholesterol": random.randint(125, 200),
+        "Oxygen Saturation": random.randint(95, 100),
+    }
+    return Bronchiectasis
+
+
+def CongenitalHeartDefect():
+    CongenitalHeartDefect = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(90, 120),
+        "Respiration": random.randint(12, 16),
+        "Glucose": random.randint(72, 140),
+        "Heart Rate": random.randint(45, 60),
+        "Cholesterol": random.randint(200, 270),
+        "Oxygen Saturation": random.randint(95, 100),
+    }
+    return CongenitalHeartDefect
+
+
+def Hypoxemia():
+    Hypoxemia = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(90, 120),
+        "Respiration": random.randint(12, 16),
+        "Glucose": random.randint(72, 140),
+        "Heart Rate": random.randint(60, 100),
+        "Cholesterol": random.randint(125, 200),
+        "Oxygen Saturation": random.randint(50, 96),
+    }
+    return Hypoxemia
+
+
+def AcuteAsthma():
+    AcuteAsthma = {
+        "Steps": random.randint(4000, 10000),
+        "Body Temperature": (round(random.uniform(35.5, 37.5), 1)),
+        "Blood Pressure": random.randint(90, 120),
+        "Respiration": random.randint(20, 30),
+        "Glucose": random.randint(72, 140),
+        "Heart Rate": random.randint(60, 100),
+        "Cholesterol": random.randint(125, 200),
+        "Oxygen Saturation": random.randint(92, 95),
+    }
+    return AcuteAsthma
+
 
 class MedicalData(Resource):
     def get(self, state):
-        diseases = {'A': AcuteAsthma, 'B': Bronchiectasis,'C':CongenitalHeartDefect,'D':Diabetes,'H':Hypoxemia,'N':MedRecN,'P':Prediabetes}
+        diseases = {"A": AcuteAsthma(), 'B': Bronchiectasis(), 'C': CongenitalHeartDefect(
+        ), 'D': Diabetes(), 'H': Hypoxemia(), 'N': MedRecN(), 'P': Prediabetes()}
         return diseases[state]
-    
- 
-api.add_resource(MedicalData,'/medidata/<string:state>')
 
-if __name__ == "__main__":    
+
+api.add_resource(MedicalData, '/medidata/<string:state>')
+
+if __name__ == "__main__":
     app.run()
